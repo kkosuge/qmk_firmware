7ab45941943fa634bbec104a51d6d68414dcfd92
dogura <daiki.ogura@soliton.co.jp>
2016-04-18 19:13:22 +0900

    move space on left
    and enable default ctrl

--- keyboard/ergodox_ez/keymaps/hachibee_osx/keymap.c
+++ keyboard/ergodox_ez/keymaps/hachibee_osx/keymap.c
@@ -37,9 +37,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_MINS,        KC_1,      KC_2,    KC_3,    KC_4,    KC_5,   KC_LEFT,
         KC_TAB,         KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,   TG(1),
-        KC_BSPC,        KC_A,      KC_S,    KC_D,    KC_F,    KC_G,
+        KC_LCTRL,       KC_A,      KC_S,    KC_D,    KC_F,    KC_G,
         LT(SYMB,KC_GRV),KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,   ALL_T(KC_NO),
-        KC_LSFT,        KC_QUOT,   KC_LALT, GUI_T(KC_KANA), CTL_T(KC_ESC),
+        KC_LSFT,        KC_QUOT,   KC_LALT, GUI_T(KC_KANA), CTL_T(KC_SPC),
 
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TG(1),       KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,    KC_K,    KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
              MEH_T(KC_NO),KC_N,   KC_M,    KC_COMM, KC_DOT, CTL_T(KC_SLSH),   KC_FN1,
-                                  CTL_T(KC_SPC), GUI_T(KC_HENK),KC_LBRC,KC_RBRC, KC_RSFT,
+                                  CTL_T(KC_ESC), GUI_T(KC_HENK),KC_LBRC,KC_RBRC, KC_RSFT,
 
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
