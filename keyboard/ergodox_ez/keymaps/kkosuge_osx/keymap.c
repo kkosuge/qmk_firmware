bed7fca00990659aad4c9f7f547a23a36497b782
kkosuge <root@kksg.net>
2016-05-08 11:30:12 +0900

    auto commit

--- keyboard/ergodox_ez/keymaps/kkosuge_osx/keymap.c
+++ keyboard/ergodox_ez/keymaps/kkosuge_osx/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| EI   |           | Kana |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | ALT  |  F7  |  =   |  '"  |   EI |                                       | Kana | Left | Down |  Up  | Right  |
+ *   |AltShf|  F7  |  =   |  '"  |   EI |                                       | Kana | Left | Down |  Up  | Right  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,--------------.
  *                                        | Prev | Next |       | VolUp | Play |
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,    KC_Q,         KC_W,    KC_E,   KC_R,   KC_T,   KC_LGUI,
         KC_LCTRL,  KC_A,         KC_S,    KC_D,   KC_F,   KC_G,
         KC_LSHIFT, CTL_T(KC_Z),  KC_X,    KC_C,   KC_V,   KC_B,   LCTL(LSFT(KC_SCLN)),
-        KC_LALT,    KC_F7,     KC_EQL,       KC_QUOT, LCTL(RSFT(KC_SCLN)),
+        LALT(KC_LSFT),    KC_F7,     KC_EQL,       KC_QUOT, LCTL(RSFT(KC_SCLN)),
                                               KC_MPRV,  KC_MNXT,
                                                                  TG(1),
                                               KC_SPC,   KC_LGUI, TG(2),
