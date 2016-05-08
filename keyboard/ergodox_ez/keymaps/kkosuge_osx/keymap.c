f651c90a1e0f37f64cbc80202978fffea2fc794c
kkosuge <root@kksg.net>
2016-05-08 11:40:12 +0900

    auto commit

--- keyboard/ergodox_ez/keymaps/kkosuge_osx/keymap.c
+++ keyboard/ergodox_ez/keymaps/kkosuge_osx/keymap.c
@@ -15,11 +15,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | Esc    |   1  |   2  |   3  |   4  |   5  | LEFT |           |   6  |   7  |   8  |   9  |   0  |  -   | Del    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |  P   | @      |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |  P   | '"     |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |        |
+ * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   | [      |
  * |--------+------+------+------+------+------| EI   |           | Kana |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   |        |
+ * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | ]      |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |AltShf|  F7  |  =   |  '"  |   EI |                                       | Kana | Left | Down |  Up  | Right  |
  *   `----------------------------------'                                       `----------------------------------'
@@ -39,15 +39,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,    KC_Q,         KC_W,    KC_E,   KC_R,   KC_T,   KC_LGUI,
         KC_LCTRL,  KC_A,         KC_S,    KC_D,   KC_F,   KC_G,
         KC_LSHIFT, CTL_T(KC_Z),  KC_X,    KC_C,   KC_V,   KC_B,   LCTL(LSFT(KC_SCLN)),
-        LALT(KC_LSFT),    KC_F7,     KC_EQL,       KC_QUOT, LCTL(RSFT(KC_SCLN)),
+        LALT(KC_LSFT),  KC_F7,   KC_EQL,  KC_QUOT, LCTL(RSFT(KC_SCLN)),
                                               KC_MPRV,  KC_MNXT,
                                                                  TG(1),
                                               KC_SPC,   KC_LGUI, TG(2),
         // right hand
         KC_6,       KC_7, KC_8,    KC_9,    KC_0,    KC_MINS, KC_BSPC,
-        KC_LGUI,    KC_Y, KC_U,    KC_I,    KC_O,    KC_P,    KC_AT,
-        KC_H,       KC_J, KC_K,    KC_L,    KC_SCLN, KC_LGUI,
-        RCTL(RSFT(KC_J)), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_RSFT,
+        KC_LGUI,    KC_Y, KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
+        KC_H,       KC_J, KC_K,    KC_L,    KC_SCLN, KC_LBRC,
+        RCTL(RSFT(KC_J)), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_RBRC,
         RCTL(RSFT(KC_J)), KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT,
         KC_VOLU, KC_MPLY,
         KC_VOLD,
