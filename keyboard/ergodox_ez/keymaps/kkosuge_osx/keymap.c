a4f295f974f2ca6fa0cefe2fba9e901d5046633c
kkosuge <root@kksg.net>
2016-05-13 22:48:06 +0900

    auto commit

--- keyboard/ergodox_ez/keymaps/kkosuge_osx/keymap.c
+++ keyboard/ergodox_ez/keymaps/kkosuge_osx/keymap.c
@@ -15,20 +15,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | Esc    |   1  |   2  |   3  |   4  |   5  | LEFT |           |   6  |   7  |   8  |   9  |   0  |  -   | Del    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |  P   | '"     |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |   =  |   Y  |   U  |   I  |   O  |  P   | '"     |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   | [      |
  * |--------+------+------+------+------+------| EI   |           | Kana |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | ]      |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |AltShf|  F7  |  =   |  '"  |   EI |                                       | Kana | Left | Down |  Up  | Right  |
+ *   |  L1  |  L2  |  F7  |      |   EI |                                       | Kana | Left | Down |  Up  | Right  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,--------------.
  *                                        | Prev | Next |       | VolUp | Play |
  *                                 ,------|------|------|       |-------+--------+------.
- *                                 |      |      | L1   |       |VolDown|        |      |
- *                                 | Space| LCmd |------|       |-------|  Tab   |Enter |
- *                                 |      |      | L2   |       | Mute  |        |      |
+ *                                 |      |      |      |       |VolDown|        |      |
+ *                                 | Space| LCmd |------|       |-------|  Del   |Enter |
+ *                                 |      |      |AltShf|       | Mute  |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -39,19 +39,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,    KC_Q,         KC_W,    KC_E,   KC_R,   KC_T,   KC_LGUI,
         KC_LCTRL,  KC_A,         KC_S,    KC_D,   KC_F,   KC_G,
         KC_LSHIFT, CTL_T(KC_Z),  KC_X,    KC_C,   KC_V,   KC_B,   LCTL(LSFT(KC_SCLN)),
-        LALT(KC_LSFT),  KC_F7,   KC_EQL,  KC_QUOT, LCTL(RSFT(KC_SCLN)),
+        TG(1), TG(2), KC_F7,  KC_QUOT, LCTL(RSFT(KC_SCLN)),
                                               KC_MPRV,  KC_MNXT,
-                                                                 TG(1),
-                                              KC_SPC,   KC_LGUI, TG(2),
+                                                                 LALT(KC_LSFT),
+                                              KC_SPC,   KC_LGUI, LALT(KC_LSFT),
         // right hand
-        KC_6,       KC_7, KC_8,    KC_9,    KC_0,    KC_MINS, KC_BSPC,
-        KC_LGUI,    KC_Y, KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
+        KC_6,       KC_7, KC_8,    KC_9,    KC_0,    KC_MINS, KC_PIPE,
+        KC_EQL,     KC_Y, KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
         KC_H,       KC_J, KC_K,    KC_L,    KC_SCLN, KC_LBRC,
         RCTL(RSFT(KC_J)), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_RBRC,
         RCTL(RSFT(KC_J)), KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT,
         KC_VOLU, KC_MPLY,
         KC_VOLD,
-        KC_MUTE, KC_TAB, KC_ENT
+        KC_MUTE, KC_BSPC, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
