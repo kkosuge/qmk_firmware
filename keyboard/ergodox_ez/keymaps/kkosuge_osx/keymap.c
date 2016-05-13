f14c0f5714b4e640c4098a31887420ef151b2dc5
kkosuge <root@kksg.net>
2016-05-14 07:44:14 +0900

    auto commit

--- keyboard/ergodox_ez/keymaps/kkosuge_osx/keymap.c
+++ keyboard/ergodox_ez/keymaps/kkosuge_osx/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |      |           |   =  |   Y  |   U  |   I  |   O  |  P   | '"     |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   | [      |
- * |--------+------+------+------+------+------| EI   |           | Kana |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| EI   |           |   \  |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | ]      |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |  L1  |  L2  |  F7  |      |   EI |                                       | Kana | Left | Down |  Up  | Right  |
@@ -44,10 +44,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                  LALT(KC_LSFT),
                                               KC_SPC,   KC_LGUI, LALT(KC_LSFT),
         // right hand
-        KC_6,       KC_7, KC_8,    KC_9,    KC_0,    KC_MINS, KC_PIPE,
-        KC_EQL,     KC_Y, KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
-        KC_H,       KC_J, KC_K,    KC_L,    KC_SCLN, KC_LBRC,
-        RCTL(RSFT(KC_J)), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_RBRC,
+        KC_6,    KC_7, KC_8,    KC_9,    KC_0,    KC_MINS, KC_PIPE,
+        KC_EQL,  KC_Y, KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
+                 KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_LBRC,
+        KC_BSLS, KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH,   KC_RBRC,
         RCTL(RSFT(KC_J)), KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT,
         KC_VOLU, KC_MPLY,
         KC_VOLD,
